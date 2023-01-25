aaef2a073c71a5255911202540809d6f83e38fdf
Tanaev Yury <Quacevizz@yandex.ru>
2023-01-25 16:12:42 +0300

    second try

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+
+let editButton = document.querySelector(".profile__edit-button");
+let popupOpener = document.querySelector(".popup");
+let formElement = document.querySelector(".popup__container"); //Разобраться почему не работает с .popup__form
+let nameInput = document.querySelector(".popup__field_value_name");
+let jobInput = document.querySelector(".popup__field_value_description");
+let valueName = document.querySelector(".profile__title");
+let valueJob = document.querySelector(".profile__subtitle");
+let popupButtonClose = document.querySelector(".popup__close");
+//открытие попапа
+function popupOpen() {
+  popupOpener.classList.add("popup_opened");
+  valueName.textContent = nameInput.value;
+
+  valueJob.textContent = jobInput.value;
+}
+// закрытие попапа
+function popupClose() {
+  popupOpener.classList.remove("popup_opened");
+}
+//Сохранение значений
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  valueName.textContent = nameInput.value;
+
+  valueJob.textContent = jobInput.value;
+
+  popupClose();
+}
+editButton.addEventListener("click", popupOpen);
+popupButtonClose.addEventListener("click", popupClose);
+formElement.addEventListener("submit", formSubmitHandler);
